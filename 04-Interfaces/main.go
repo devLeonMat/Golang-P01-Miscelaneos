d713af95a10742711858439d8cf1aa5715ea94fe
leon matias <leonmatias.work@gmail.com>
2021-03-15 12:43:11 -0500

    add second case

--- 04-Interfaces/main.go
+++ 04-Interfaces/main.go
@@ -26,36 +26,64 @@ type man struct {
 	breathing bool
 	thinking  bool
 	eating    bool
+	isMan     bool
 }
 type woman struct {
-	age       int
-	height    float32
-	weight    float32
-	breathing bool
-	thinking  bool
-	eating    bool
+	man
 }
 
-func (h *man) breathe()    { h.breathing = true }
-func (h *man) eat()        { h.eating = true }
-func (h *man) think()      { h.thinking = true }
-func (h *man) sex() string { return "man" }
-
-func (w *woman) breathe()    { w.breathing = true }
-func (w *woman) eat()        { w.eating = true }
-func (w *woman) think()      { w.thinking = true }
-func (w *woman) sex() string { return "woman" }
+func (h *man) breathe() { h.breathing = true }
+func (h *man) eat()     { h.eating = true }
+func (h *man) think()   { h.thinking = true }
+func (h *man) sex() string {
+	if h.isMan == true {
+		return "man"
+	} else {
+		return "woman"
+	}
+}
 
 func HumanBreath(hu human) {
 	hu.breathe()
 	fmt.Printf("I'm a %s, and I'm already breathing \n", hu.sex())
 }
 
-func main() {
-	Pedro := new(man)
-	HumanBreath(Pedro)
+/*************** ANIMAL ***/
+type dog struct {
+	breathing bool
+	eating    bool
+	carnivore bool
+}
 
-	Maria := new(woman)
-	HumanBreath(Maria)
+func (d *dog) breathe()          { d.breathing = true }
+func (d *dog) eat()              { d.eating = true }
+func (d *dog) isCarnivore() bool { return d.carnivore }
+
+func AnimalBreath(an animal) {
+	an.breathe()
+	fmt.Println("I'm a animal and I'm already breathing")
+}
+
+func AnimalCarnivore(an animal) int {
+	if an.isCarnivore() == true {
+		return 1
+	}
+	return 0
+}
+
+func main() {
+	totalCarnivores := 0
+	Dogo := new(dog)
+	Dogo.carnivore = true
+	AnimalBreath(Dogo)
+	totalCarnivores = +AnimalCarnivore(Dogo)
+
+	fmt.Printf("Total carnivores %d", totalCarnivores)
+	//Pedro := new(man)
+	//Pedro.isMan = true
+	//HumanBreath(Pedro)
+	//
+	//Maria := new(woman)
+	//HumanBreath(Maria)
 
 }
